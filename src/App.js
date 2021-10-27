f3a3f9bbd18fa78091d751e502df86070e75c948
Mark R Baker <mrbaker1917@gmail.com>
2021-10-27 16:10:17 -0700

    adds function to textarea

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-
+import React, { useState } from 'react'
 import "./App.css";
 
 function App() {
+
+  const [ typetext, updateTypetext ] = useState("");
+
+  function handleChange(e) {
+    const {value} = e.target;
+
+    updateTypetext(value)
+  }
+
   return (
     <div>
-      <h1>Speed Typing Game</h1>
-      <textarea value="" />
-      <h4>Time remaining: </h4>
+      <h1>Speed Typing Game: How fast can you type?</h1>
+      <textarea value={typetext} onChange={handleChange} />
+      <h4>Time Remaining: </h4>
       <button>Start</button>
-      <h1>Word count:</h1>
+      <h1>Word Count:</h1>
     </div>
   );
 }
