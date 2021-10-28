66963c9c51e8734bda9c2bd0d1615e363c68d264
Mark R Baker <mrbaker1917@gmail.com>
2021-10-28 14:32:14 -0700

    adds countdown to app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-
-  const [ typetext, updateTypetext ] = useState("");
+  const [typetext, updateTypetext] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
 
   function handleChange(e) {
-    const {value} = e.target;
+    const { value } = e.target;
 
-    updateTypetext(value)
+    updateTypetext(value);
   }
 
   function countWords(text) {
-    const words_arr = text.trim().split(" ")
-    const filteredWords = words_arr.filter(word => word !== "")
-    console.log(filteredWords.length)
+    const words_arr = text.trim().split(" ");
+    const filteredWords = words_arr.filter((word) => word !== "");
+    console.log(filteredWords.length);
     return filteredWords.length;
   }
+
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    }
+  }, [timeRemaining]);
 
   return (
     <div>
       <h1>Speed Typing Game: How fast can you type?</h1>
       <textarea value={typetext} onChange={handleChange} />
-      <h4>Time Remaining: </h4>
+      <h4>Time Remaining: {timeRemaining}</h4>
       <button onClick={() => countWords(typetext)}>Start</button>
       <h1>Word Count:</h1>
     </div>
