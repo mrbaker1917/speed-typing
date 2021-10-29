10b170c3e8390da17caaf21f46ce714e59368240
Mark R Baker <mrbaker1917@gmail.com>
2021-10-29 13:27:40 -0700

    adds start button func and clear

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const STARTING_TIME = 5
   const [typetext, updateTypetext] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(STARTING_TIME);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0)
 
   function handleChange(e) {
     const { value } = e.target;
@@ -19,13 +21,25 @@ function App() {
     return filteredWords.length;
   }
 
+  function startGame() {
+    setIsTimeRunning(true)
+    setTimeRemaining(STARTING_TIME)
+    updateTypetext("")
+    setWordCount(0)
+  }
+
+  function endGame() {
+    setIsTimeRunning(false)
+    setWordCount(countWords(typetext))
+  }
+
   useEffect(() => {
     if (isTimeRunning && timeRemaining > 0) {
       setTimeout(() => {
         setTimeRemaining((time) => time - 1);
       }, 1000);
     } else if (timeRemaining === 0) {
-        setIsTimeRunning(false)
+        endGame()
       }
   }, [timeRemaining, isTimeRunning])
 
@@ -34,8 +48,8 @@ function App() {
       <h1>Speed Typing Game: How fast can you type?</h1>
       <textarea value={typetext} onChange={handleChange} />
       <h4>Time Remaining: {timeRemaining}</h4>
-      <button onClick={() => setIsTimeRunning(true)}>Start</button>
-      <h1>Word Count:</h1>
+      <button onClick={() => startGame()}>Start</button>
+      <h1>Word Count: {wordCount}</h1>
     </div>
   );
 }
